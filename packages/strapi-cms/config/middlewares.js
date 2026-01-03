3d4f476d2159b3d52b520232d9d6242c5b0a52cc
devonjamalwallace-commits <devonjamalwallace@gmail.com>
2026-01-02 21:47:32 -0500

    Simplify Strapi config

--- packages/strapi-cms/config/middlewares.js
+++ packages/strapi-cms/config/middlewares.js
@@ -1,47 +1,12 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: ['Content-Type', 'Authorization', 'X-Site-ID', 'Origin', 'Accept'],
-      origin: [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://localhost:3002',
-        'http://localhost:3003',
-        'https://mayhemworld.io',
-        'https://nexus-ai.io',
-        'https://goddessesofatl.com',
-        'https://shotbymayhem.com',
-      ],
-    },
-  },
+  'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'global::site-filter',
-    config: {
-      requireSiteId: false,
-    },
-  },
 ];
