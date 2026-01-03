fd593a4d883fb258f9d73ad242488df77a56f065
devonjamalwallace-commits <devonjamalwallace@gmail.com>
2026-01-02 21:39:03 -0500

    Remove Cloudinary and SendGrid - use defaults

--- packages/strapi-cms/config/plugins.js
+++ packages/strapi-cms/config/plugins.js
@@ -1,39 +1,4 @@
 module.exports = ({ env }) => ({
-  // Upload - Cloudinary for high-res photography needs
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {
-          folder: env('CLOUDINARY_FOLDER', 'mayhem'),
-        },
-        uploadStream: {
-          folder: env('CLOUDINARY_FOLDER', 'mayhem'),
-        },
-        delete: {},
-      },
-    },
-  },
-
-  // Email - SendGrid for transactional emails
-  email: {
-    config: {
-      provider: 'sendgrid',
-      providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
-      },
-      settings: {
-        defaultFrom: env('EMAIL_DEFAULT_FROM', 'noreply@mayhemworld.io'),
-        defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO', 'support@mayhemworld.io'),
-      },
-    },
-  },
-
   // GraphQL API
   graphql: {
     enabled: true,
@@ -45,5 +10,4 @@ module.exports = ({ env }) => ({
       amountLimit: 100,
     },
   },
-
 });
